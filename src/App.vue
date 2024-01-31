6d3f7798e91d8e5cfb6a64f94bcfc2a4cc628249
Elyes.Benkhoud <elyesbenkhoud377@gmail.com>
2024-01-31 10:06:47 +0200

    notification - delete fixed

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,17 @@
 <script setup>
 import { ref, onMounted, computed, watch } from 'vue';
+import Toastify from 'toastify-js';
+import 'toastify-js/src/toastify.css';
+
+const showToast = (message, duration, color) => {
+  Toastify({
+    text: message,
+    duration: duration,
+    gravity: 'top',
+    position: 'right',
+    backgroundColor: color,
+  }).showToast();
+};
 
 const todos = ref([]);
 const name = ref('');
@@ -7,11 +19,9 @@ const name = ref('');
 const input_content = ref('');
 const input_category = ref(null);
 
-
 watch(name, (newVal) => {
   localStorage.setItem('name', newVal);
 });
-
 
 const addTodo = () => {
   if (input_content.value.trim() === '' || input_category.value === null) {
@@ -28,20 +38,44 @@ const addTodo = () => {
 
   todos.value.push(newTodo);
   addTodoAndLoadLogo(newTodo);
-
-  // Clear the input values
+  showToast('Todo added successfully!', 2500, 'linear-gradient(to right, #00b09b, #96c93d)');
   input_content.value = '';
   input_category.value = null;
 };
 
-const removeTodo = (todo) => {
+const removeTodoAndLogo = (todo) => {
   todos.value = todos.value.filter((t) => t !== todo);
+
+  if (groupedTodosLocalStorage.value[todo.category]) {
+    const todoIndex = groupedTodosLocalStorage.value[todo.category].todos.indexOf(todo);
+    if (todoIndex !== -1) {
+      groupedTodosLocalStorage.value[todo.category].todos.splice(todoIndex, 1);
+
+      if (groupedTodosLocalStorage.value[todo.category].todos.length === 0) {
+        delete groupedTodosLocalStorage.value[todo.category];
+      }
+    }
+
+    // Save the updated groupedTodosLocalStorage to localStorage
+    localStorage.setItem('groupedTodos', JSON.stringify(groupedTodosLocalStorage.value));
+  }
 };
+
+
+const removeTodo = (todo) => {
+  removeTodoAndLogo(todo);
+  showToast('Todo removed successfully!');
+};
+
+const groupedTodosLocalStorage = ref(JSON.parse(localStorage.getItem('groupedTodos')) || {});
+
+watch(groupedTodosLocalStorage, (newVal) => {
+  localStorage.setItem('groupedTodos', JSON.stringify(newVal));
+});
 
 onMounted(() => {
   name.value = localStorage.getItem('name') || '';
-  // Load groupedTodos from localStorage on mount
-  groupedTodos.value = JSON.parse(localStorage.getItem('groupedTodos')) || {};
+  groupedTodosLocalStorage.value = JSON.parse(localStorage.getItem('groupedTodos')) || {};
 });
 
 const loadLogo = async (category) => {
@@ -49,125 +83,114 @@ const loadLogo = async (category) => {
   return logoModule.default;
 };
 
-const groupedTodos = ref({});
+const groupedTodos = computed(() => Object.values(groupedTodosLocalStorage.value));
 
 const addTodoAndLoadLogo = async (todo) => {
-  if (!groupedTodos.value[todo.category]) {
-    groupedTodos.value[todo.category] = {
+  if (!groupedTodosLocalStorage.value[todo.category]) {
+    groupedTodosLocalStorage.value[todo.category] = {
       name: todo.category,
       logoPath: '',
       todos: [],
     };
 
-    // Asynchronously load the logo and update logoPath
     try {
       const logoPath = await loadLogo(todo.category);
-      groupedTodos.value[todo.category].logoPath = logoPath;
+      groupedTodosLocalStorage.value[todo.category].logoPath = logoPath;
     } catch (error) {
       console.error(`Error loading logo for ${todo.category}:`, error);
     }
   }
 
-  groupedTodos.value[todo.category].todos.push(todo);
-
-  // Save groupedTodos to localStorage after each update
-  localStorage.setItem('groupedTodos', JSON.stringify(groupedTodos.value));
+  groupedTodosLocalStorage.value[todo.category].todos.push(todo);
 };
-
-const computedGroupedTodos = computed(() => Object.values(groupedTodos.value));
 
 </script>
 
-
 <template>
-	<main class="app">
-		
-		<section class="greeting">
-			<h2 class="title">
-				Bine ati venit, <input type="text" id="name" placeholder="numele tale" v-model="name">
-			</h2>
-		</section>
+  <main class="app">
+    <section class="greeting">
+      <h2 class="title">
+        Bine ati venit, <input type="text" id="name" placeholder="numele tale" v-model="name">
+      </h2>
+    </section>
 
-		<section class="create-todo">
-			<h3>Cumparaturi list !!</h3>
+    <section class="create-todo">
+      <h3>Cumparaturi list !!</h3>
 
-			<form id="new-todo-form" @submit.prevent="addTodo">
-				<h4>Ce vrei sa cumperi ?</h4>
-				<input 
-					type="text" 
-					name="content" 
-					id="content" 
-					placeholder="e.g. Fructe de mare"
-					v-model="input_content" />
-				
-				<h4>Alege magazinul </h4>
-				<div class="options">
+      <form id="new-todo-form" @submit.prevent="addTodo">
+        <h4>Ce vrei sa cumperi ?</h4>
+        <input
+          type="text"
+          name="content"
+          id="content"
+          placeholder="e.g. Fructe de mare"
+          v-model="input_content"
+        />
 
-					<label>
-						<input 
-							type="radio" 
-							name="category" 
-							id="category1" 
-							value="kaufland"
-							v-model="input_category" />
-						<span class="bubble kaufland"></span>
-						<div class="brand"> <img src='./assets/kauflandLogo.png' alt="kaufland logo" class="store-logo" /></div>
-						
-					</label>
+        <h4>Alege magazinul </h4>
+        <div class="options">
+          <label>
+            <input
+              type="radio"
+              name="category"
+              id="category1"
+              value="kaufland"
+              v-model="input_category"
+            />
+            <span class="bubble kaufland"></span>
+            <div class="brand"> <img src='./assets/kauflandLogo.png' alt="kaufland logo" class="store-logo" /></div>
 
-					<label>
-						<input 
-							type="radio" 
-							name="category" 
-							id="category2" 
-							value="lidl"
-							v-model="input_category" />
-						<span class="bubble lidl"></span>
-						<div class="brand">	<img src='./assets/lidlLogo.png' alt="lidl logo" class="store-logo" /> </div>
-					
-					</label>
+          </label>
 
-					<label>
-						<input 
-							type="radio" 
-							name="category" 
-							id="category3" 
-							value="auchan"
-							v-model="input_category" />
-						<span class="bubble auchan"></span>
-						<div class="brand"><img src='./assets/auchanLogo.png' alt="auchan logo" class="store-logo" />  </div>
-							
-					</label>
+          <label>
+            <input
+              type="radio"
+              name="category"
+              id="category2"
+              value="lidl"
+              v-model="input_category"
+            />
+            <span class="bubble lidl"></span>
+            <div class="brand"> <img src='./assets/lidlLogo.png' alt="lidl logo" class="store-logo" /> </div>
+          </label>
 
-				</div>
+          <label>
+            <input
+              type="radio"
+              name="category"
+              id="category3"
+              value="auchan"
+              v-model="input_category"
+            />
+            <span class="bubble auchan"></span>
+            <div class="brand"> <img src='./assets/auchanLogo.png' alt="auchan logo" class="store-logo" /> </div>
+          </label>
+        </div>
+        <input type="submit" value="Add todo" />
+      </form>
+    </section>
 
-				<input type="submit" value="Add todo" />
-			</form>
-		</section>
+    <section class="todo-list">
+      <div class="list" id="todo-list">
+        <div v-for="magazine in groupedTodos" :key="magazine.name">
+          <img :src="magazine.logoPath" alt="Magazine Logo" class="store-logo" />
 
+          <div v-for="todo in magazine.todos" :class="`todo-item ${todo.done && 'done'}`">
+            <label>
+              <input type="checkbox" v-model="todo.done" />
+              <span :class="`bubble ${todo.category}`"></span>
+            </label>
 
-		<section class="todo-list">
-			<div class="list" id="todo-list">
-				<div v-for="magazine in computedGroupedTodos" :key="magazine.name">
-					<img :src="magazine.logoPath" alt="Magazine Logo" class="store-logo" />
-				
-					<div v-for="todo in magazine.todos" :class="`todo-item ${todo.done && 'done'}`">
-						<label>
-							<input type="checkbox" v-model="todo.done" />
-							<span :class="`bubble ${todo.category}`"></span>
-						</label>
+            <div class="todo-content">
+              <input type="text" v-model="todo.content" />
+            </div>
 
-						<div class="todo-content">
-							<input type="text" v-model="todo.content" />
-						</div>
-
-						<div class="actions">
-							<button class="delete" @click="removeTodo(todo)">Delete</button>
-						</div>
-					</div>
-				</div>
-			</div>
-		</section>
-
-	</main>
+            <div class="actions">
+              <button class="delete" @click="removeTodo(todo)">Delete</button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  </main>
 </template>
